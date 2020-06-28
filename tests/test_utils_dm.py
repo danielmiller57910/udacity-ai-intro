d643195740f6ae6c4ecef937d39b008488674254
Daniel Miller <daniel.miller@sainsburys.co.uk>
2020-06-28 15:31:17 +0100

    board finished

--- tests/test_utils_dm.py
+++ tests/test_utils_dm.py
@@ -1,6 +1,6 @@
 import utils_dm
 import unittest
-
+SAMPLE_GRID = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
 class TestDMUtils(unittest.TestCase):
 
     def test_get_grid_columns_and_rows(self):
@@ -8,4 +8,10 @@ class TestDMUtils(unittest.TestCase):
         self.assertEqual(grid_cols[0], 1)
         self.assertEqual(grid_cols[-1], 9)
         self.assertEqual(grid_rows[0], 'A')
-        self.assertEqual(grid_rows[-1], 'J')
+        self.assertEqual(grid_rows[-1], 'I')
+    
+    def test_setup_grid(self):
+        grid_board = utils_dm.grid_values(SAMPLE_GRID)
+        self.assertEqual(grid_board['A1'], '.')
+        self.assertEqual(grid_board['A3'], '3') 
+        self.assertEqual(grid_board['I9'], '.')
--- utils_dm.py
+++ utils_dm.py
@@ -1,12 +1,19 @@
 from string import ascii_letters
-
-def string_to_grid():
-    grid_columns = get_grid_columns()
-    grid_rows = get_grid_rows() 
-    return True
+import pdb
+def grid_values(grid: str) -> dict:
+    sudoku_board_dictionary = {}
+    grid_rows, grid_columns = get_grid_rows(), get_grid_columns()
+    grid_index = 0
+    for row in grid_rows:
+        for col in grid_columns:
+           sudoku_board_dictionary[row+str(col)] = grid[grid_index]
+           grid_index +=1
+    
+    return sudoku_board_dictionary
 
 def get_grid_columns():
     return [i for i in range(1, 10)]
 
 def get_grid_rows():
     return [ascii_letters[ascii_letters.index('A') + i] for i in range(0, 9)]
+
