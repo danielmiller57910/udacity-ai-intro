9a0b9aeeb45c52b9cce0f3464956287f11bfeb76
Daniel Miller <daniel.miller@sainsburys.co.uk>
2020-07-09 20:12:58 +0100

    associated nodes done

--- solution.py
+++ solution.py
@@ -1,20 +1,8 @@
-
 from utils import *
-
-
-row_units = [cross(r, cols) for r in rows]
-column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
-
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
-
-
-# Must be called after all units (including diagonals) are added to the unitlist
-units = extract_units(unitlist, boxes)
-peers = extract_peers(units, boxes)
-
+from string import ascii_letters
+from objects.associated_nodes import AssociatedNodes
+import pdb
+import copy
 
 def naked_twins(values):
     """Eliminate values using the naked twins strategy.
@@ -57,110 +45,147 @@ def naked_twins(values):
     raise NotImplementedError
 
 
-def eliminate(values):
-    """Apply the eliminate strategy to a Sudoku puzzle
+from utils import *
+from string import ascii_letters
+import pdb
+import copy
 
-    The eliminate strategy says that if a box has a value assigned, then none
-    of the peers of that box can have the same value.
+def create_grid_columns():
+    return [i for i in range(1, 10)]
 
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
+def create_grid_rows():
+    return [ascii_letters[ascii_letters.index('A') + i] for i in range(0, 9)]
 
-    Returns
-    -------
-    dict
-        The values dictionary with the assigned values eliminated from peers
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+def empty_grid_indexes(grid: dict) -> dict:
+    return [index for index, value in grid.items() if value == '.']
+
+def get_unique_board_set(associated_nodes):
+    associated_nodes.build_set()
+    return associated_nodes.return_unique_board_set()
 
 
-def only_choice(values):
-    """Apply the only choice strategy to a Sudoku puzzle
 
-    The only choice strategy says that if only one box in a unit allows a certain
-    digit, then that box must be assigned that digit.
+class EmptyNode:
+    
+    ALLOWED_VALUES = [i for i in range(1, 10)]
 
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
+    def __init__(self, node: str, associated_indexes: list, grid: dict):
+        self.node = node
+        self.associated_indexes = associated_indexes
+        self.grid = grid
+        self.value_range = self._identify_filled_values()
+    
+    def _identify_filled_values(self):
+        associated_values = [self.grid[i] for i in self.associated_indexes]
+        return [int(val) for val in associated_values if val != '.']
+    
+    def get_value_range(self):
+        return ''.join([str(val) for val in self.ALLOWED_VALUES])
 
-    Returns
-    -------
-    dict
-        The values dictionary with all single-valued boxes assigned
+class NodeEliminator:
+    def __init__(self, grid: dict) -> dict:
+        self.grid = grid
+        self.completed_nodes = self._find_completed_nodes()
 
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    def remove_completed_indexes_from_grid(self):
+        for node in self.completed_nodes:
+            value_for_elimination = node["value"]
+            for index in node["associated_nodes"]:
+                self.grid[index] = self._remove_value(value_for_elimination, self.grid[index])
+        return self.grid
+    
+    def _find_completed_nodes(self):
+        completed_nodes = []
+        for k, v in self.grid.items():
+            if len(v) == 1:
+                completed_nodes.append({
+                    "index": k, 
+                    "value": v, 
+                    "associated_nodes": get_unique_board_set(AssociatedNodes(k))
+               })
+        return completed_nodes
+    
+    def _remove_value(self, value_for_elimination, options_string):
+        return options_string.replace(str(value_for_elimination), "")
+        
 
+class NodeOnlyChoice:
+    def __init__(self, grid: dict) -> dict:
+        self.grid = grid
+        self.incomplete_nodes = self._find_incomplete_nodes()
+
+    def eliminate(self):
+        for i in self.incomplete_nodes:
+            current_index, current_value = i['index'], i['value']
+            for j in i["associated_nodes"]:
+                if len(self.grid[j]) == 1:
+                    impossible_value = str(self.grid[j])
+                    if impossible_value in current_value:
+                        current_value = current_value.replace(impossible_value, "")
+                    self.grid[current_index] = current_value
+        return self.grid
+    
+    def _find_incomplete_nodes(self):
+        incomplete_nodes = []
+        for k, v in self.grid.items():
+            if len(v) > 1:
+                incomplete_nodes.append({
+                    "index": k, 
+                    "value": v, 
+                    "associated_nodes": get_unique_board_set(AssociatedNodes(k))
+               })
+        return incomplete_nodes
+
+
+def eliminate(grid: dict) -> dict:
+    node_eliminator = NodeEliminator(grid)
+    return node_eliminator.remove_completed_indexes_from_grid()
+
+def only_choice(grid: dict) -> dict:
+    only_choice = NodeOnlyChoice(grid)
+    return only_choice.eliminate()
 
 def reduce_puzzle(values):
-    """Reduce a Sudoku puzzle by repeatedly applying all constraint strategies
-
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict or False
-        The values dictionary after continued application of the constraint strategies
-        no longer produces any changes, or False if the puzzle is unsolvable 
-    """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
-
-
-def search(values):
-    """Apply depth first search to solve Sudoku puzzles in order to solve puzzles
-    that cannot be solved by repeated reduction alone.
-
-    Parameters
-    ----------
-    values(dict)
-        a dictionary of the form {'box_name': '123456789', ...}
-
-    Returns
-    -------
-    dict or False
-        The values dictionary with all boxes assigned or False
-
-    Notes
-    -----
-    You should be able to complete this function by copying your code from the classroom
-    and extending it to call the naked twins strategy.
-    """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
-
-
-def solve(grid):
-    """Find the solution to a Sudoku puzzle using search and constraint propagation
-
-    Parameters
-    ----------
-    grid(string)
-        a string representing a sudoku grid.
-        
-        Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-
-    Returns
-    -------
-    dict or False
-        The dictionary representation of the final sudoku grid or False if no solution exists.
-    """
-    values = grid2values(grid)
-    values = search(values)
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(copy.deepcopy(values))
+        values = only_choice(copy.deepcopy(values))
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
     return values
 
+def solved(values):
+    for k, v in values.items():
+        if len(v) != 1:
+            return False
+    return True
+
+def pick(grid):
+    possible_keys = [{"key": k, "value": v} for k, v in grid.items() if len(v) != 1]
+    return sorted(possible_keys, key=lambda i: len(i['value']))[0]['key']
+
+def search(values):
+    values = reduce_puzzle(values)
+    if values is False:
+        return False 
+    
+    if solved(values): 
+        return values
+        
+    current = pick(values)
+    for value in values[current]:
+        new_sudoku = copy.deepcopy(values)
+        new_sudoku[current] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+def solve(values):
+    return search(values)
 
 if __name__ == "__main__":
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
